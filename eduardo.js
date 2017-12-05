ffd1d7fc3f4a44db9bc1b7f63c2c6cea3c90a31e
evokillz17 <eocampo0808@icloud.com>
2017-12-05 15:28:24 -0800

    Create eduardo.js

---
+++ eduardo.js
@@ -0,0 +1,94 @@
+//Draws Mountain
+function drawMountain(location) {
+ penColor(rgb(100,65,36));
+ penWidth(40);
+ penUp();
+ moveTo(location, 415);
+ turnTo(26);
+ penDown();
+ moveForward(250);
+ turnTo(154);
+ moveForward(250);
+ colorMountain();
+ drawTip();
+}
+//draws all mountains
+function drawAllmountains() {
+  penUp();
+  for (var i = 0; i < 3; i++) {
+  drawMountain(randomNumber(-150,150));
+  }
+}
+//draws snow on tips
+function drawTip() {
+  penWidth(45);
+  penRGB(236, 240, 241);
+  turnTo(154);
+  moveForward(55);
+  moveBackward(55);
+  turnTo(206);
+  moveForward(30);
+}
+function colorMountain() {
+  penWidth(40);
+  turnTo(270);
+  moveForward(185);
+  moveBackward(75);
+  turnRight();
+  moveForward(62);
+  dot(88);
+  moveForward(161);
+}
+
+//draws Background
+ function drawBackground() {
+ penRGB(44, 62, 80);
+ dot(300);
+}
+
+//draws snow floor
+function snowFloor() {
+ penRGB(236, 240, 241);
+ penWidth(60);
+ penUp();
+ moveTo(0, 450);
+ turnTo(90);
+ penDown();
+ moveForward(320);
+}
+
+function drawAuroralights(){
+  for (var i = 0; i < 65; i++) {
+  penUp();
+  moveTo(randomNumber(-10,320),randomNumber(60,100 ));
+  turnTo(randomNumber(175,180));
+  drawRay(randomNumber(70,100)); 
+  }
+}
+//draws rays for aurora
+function drawRay(size){
+  penDown();
+  penWidth(randomNumber(3,10));
+  penRGB(randomNumber(100, 190), randomNumber(120, 196), randomNumber(50, 198),0.6);
+  turnTo(0);
+  arcLeft(30,size);
+  arcRight(60,size);
+  arcLeft(60,size);
+  arcRight(60,size);
+  penUp();
+}
+//draws background stars
+function allStars() {
+  for (var i = 0; i < 20; i++) {
+  penRGB(252, 226, 95, 0.5);
+  penUp();
+  moveTo(randomNumber(-10,320),randomNumber(-5,415 ));
+  dot(randomNumber(3,5 )); 
+  }
+}
+
+drawBackground();
+allStars();
+drawAllmountains(10);
+drawAuroralights();
+snowFloor();
