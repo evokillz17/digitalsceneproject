b920314dbbdc43d3ff9528c5a621d459a3ec3114
evokillz17 <eocampo0808@icloud.com>
2017-12-05 17:29:47 -0800

    Update Natalia.js

--- Natalia.js
+++ Natalia.js
@@ -1,34 +1,30 @@
 function drawAllSnowmen() {
-  //draws two snowmen
   for (var i = 0; i < 2; i++) {
-    moveTo(randomNumber(0, 318), 415);
+    moveTo(randomNumber(0, 318), 410);
+    turnTo(0);
     drawSnowmen();
+   
   }
 }
 function drawSnowmen() {
-  penColor("white");
-  //base is the bottom of the body, body is the middle snowball, and head is the top
+  penRGB(236, 240, 241, 1);
   drawBase();
   drawBody();
   drawHead();
   drawEyes();
 }
 function drawBase() {
-  //bottom of the body
   dot(randomNumber(30, 40));
   moveForward(50);
 }
 function drawBody() {
-  //middle of the body
   dot(randomNumber(20, 23));
   moveForward(36);
 }
 function drawHead() {
-  dot(randomNumber(13, 17));
-  //the top of the body
+  dot(randomNumber(16, 19));
 }
 function drawEyes() {
-  //its t add facial features to the snowmen
   penColor("black");
   turnRight(90);
   moveForward(5);
@@ -40,16 +36,15 @@ function drawEyes() {
   turnTo(0);
 }
 function drawSnow() {
-  //transperncy is high so that we are able to tell that it is snow
   penRGB(255, 255, 255, 0.43);
   dot(6);
 }
 function drawAllSnow() {
-  //snowballs to represent active, falling snow
   for (var i = 0; i < 130; i++) {
-    moveTo(randomNumber(-5, 350), randomNumber(-5, 400));
+    moveTo(randomNumber(-5, 350), randomNumber(-5, 430));
     drawSnow();
   }
 }
+
 drawAllSnowmen();
 drawAllSnow();
